427bd17f16fd37bc921225ccf9945273c05082c7
codefromdom <126583310+codefromdom@users.noreply.github.com>
2023-04-11 09:34:09 +0200

    Update and rename heic-to-pdf.py to heic-2-pdf.py

--- heic-2-pdf.py
+++ heic-2-pdf.py
@@ -1,5 +1,6 @@
 import os
 from PIL import Image
+import shlex
 
 # Directory in which the script is executed
 directory = os.getcwd()
@@ -16,4 +17,11 @@ for filename in os.listdir(directory):
 
         # Convert the image to PDF
         pdf_filename = os.path.join(directory, f"{os.path.splitext(filename)[0]}.pdf")
-        os.system(f"convert {os.path.join(directory, f'{os.path.splitext(filename)[0]}.jpg')} {pdf_filename}")
+        cmd = f'convert "{os.path.join(directory, f"{os.path.splitext(filename)[0]}.jpg")}" "{pdf_filename}"'
+        os.system(shlex.quote(cmd))
+
+# Loop through all files in the directory
+for filename in os.listdir(directory):
+    # Check if file is not a PDF or Python script and delete it
+    if not (filename.endswith(".pdf") or filename.endswith(".py")):
+        os.remove(os.path.join(directory, filename))
